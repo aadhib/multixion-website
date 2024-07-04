3d1b10362721a876a98a71aadb872f4ac70f1f42
Zedan <zed.saheer5@gmail.com>
2024-07-04 11:03:55 +0300

    Replaced hero text with tagline

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -9,18 +9,21 @@ export default function Hero1() {
                     <div className="hero-style1">
                         <div className="row">
                             <div className="col-lg-8">
-                                <h1 className="hero-title wow img-custom-anim-left" data-wow-duration="1.5s" data-wow-delay="0.1s"><span className="text-theme">INNOVATE, INTEGRATE,
-                                    </span> INSPIRE</h1>
+                                <h1 className="hero-title wow img-custom-anim-left" data-wow-duration="1.5s" data-wow-delay="0.1s"><span className="text-theme">
+                                    INNOVATE, INTEGRATE,
+                                </span> INSPIRE</h1>
                             </div>
                             <div className="col-lg-4 align-self-end">
-                                <p className="hero-text wow img-custom-anim-right" data-wow-duration="1.5s" data-wow-delay="0.1s">Our agency is a dynamic fusion of
+                                <p className="hero-text wow img-custom-anim-right" data-wow-duration="1.5s" data-wow-delay="0.1s">
+                                    Our company is a dynamic fusion of
                                     innovation and expertise, committed to crafting visual experiences that leave an
-                                    indelible mark.</p>
+                                    indelible mark.
+                                </p>
                             </div>
                         </div>
                         <div className="tg-button-wrap">
                             <Link href="/about" className="btn border-white wow img-custom-anim-right">
-                                <img src="/assets/img/icon/star-lock.svg" alt="img" />2024 BEST AGENCY
+                                <img src="/assets/img/icon/star-lock.svg" alt="img" />VISION 2030
                             </Link>
                             <Link href="/about" className="btn border-white wow img-custom-anim-right">
                                 <i className="icon-globe" />WORLD CLASS AGENCY
