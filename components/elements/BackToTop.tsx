a9733f3b888a03b81d76d2a3e16c6eaeb8c87b55
Aadhib <aadhibnasserv.hussain@gmail.com>
2024-08-21 09:43:41 +0300

    Update BackToTop Icon

--- components/elements/BackToTop.tsx
+++ components/elements/BackToTop.tsx
@@ -1,7 +1,6 @@
 'use client'
 import { useEffect, useState } from "react"
 import Script from "next/script"
-import { redirect } from 'next/navigation'
 
 export default function BackToTop() {
     const [hasScrolled, setHasScrolled] = useState(false)
@@ -35,9 +34,16 @@ export default function BackToTop() {
             />
             {hasScrolled && (
                 <a className="scroll__top scroll-to-target open" onClick={handleClick}>
-                    Schedule a meeting?
+                    <svg 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        width="30" 
+                        height="30" 
+                        fill="white" 
+                        viewBox="0 0 24 24"
+                    >
+                        <path d="M19 4h-1V2h-2v2H8V2H6v2H5c-1.1 0-1.99.9-1.99 2L3 20c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 16H5V9h14v11zm-2-9H7v2h10V11zm0 4H7v2h10v-2z"/>
+                    </svg>
                 </a>
-
             )}
         </>
     )
